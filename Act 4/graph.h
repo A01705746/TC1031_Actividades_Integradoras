0dae6c4f57bb55093956de18547855dad6fc3a02
A01705746 <65366693+A01705746@users.noreply.github.com>
2020-12-03 04:34:35 -0600

    Add files via upload

---
+++ Act 4/graph.h
@@ -0,0 +1,198 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <fstream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Graph {
+	private:
+	vector<vector<int>> adj_list;
+	int nu_vertices;
+	int nu_edges;
+	string ruta[6]={"Queretaro", "Zacatecas", "Aguascalientes", "San Luis Potosi", "CDMX", "Hidalgo"};
+
+	public:
+	Graph(){}
+    string print_vector(vector<int> vec);
+
+	void loadGraphList(string filename, int vertices, int edges); // Cargar elementos a la lista de adyacencia
+
+	string printAdjList(); // Impresión de la lista
+
+	string DFS(int init_vertex, int target_vertex); // Algoritmos de búsqueda
+	string BFS(int init_vertex, int target_vertex);
+};
+
+string Graph:: print_vector(vector<int> vec){
+	string result = "";
+	for (int i=0; i<vec.size(); i++){
+		result = result + ruta[(vec[i])] + ", ";
+	}
+	return result;
+}
+
+void Graph::loadGraphList(string filename, int vertices, int edges){
+	nu_vertices = vertices;
+	nu_edges = edges;
+
+	for (int i=0; i<vertices; i++){     // Creación de la lista de adyacencia
+		vector<int> row;
+		adj_list.push_back(row);
+	}
+
+	ifstream file;                      // Lectura de archivo
+	file.open(filename);
+	while(file.good()){
+		string init_vertex;
+		string end_vertex;
+		getline(file, init_vertex, ',');
+		getline(file, end_vertex, '\n');
+		init_vertex = init_vertex.substr(1, init_vertex.length()-1);
+		end_vertex = end_vertex.substr(1, end_vertex.length()-2);
+		int init_node = stoi(init_vertex);
+		int end_node = stoi(end_vertex);
+
+		adj_list[init_node].push_back(end_node); // Llenado del grafo
+		adj_list[end_node].push_back(init_node);
+	}
+}
+
+string Graph::printAdjList(){
+	string result = "";
+	for(int i=0; i<adj_list.size(); i++){
+		result += ruta[i] + " llega a: ";
+		sort(adj_list[i].begin(), adj_list[i].end());
+		for(int j=0; j<adj_list[i].size(); j++){
+			result += ruta[(adj_list[i][j])] + ", ";
+		}
+		result += "\n";
+	}
+	return result;
+}
+
+
+string Graph::DFS(int init_vertex, int target_vertex){
+
+	vector<vector<int>> adj_list_copy;          // Copia de la lista de adyacencia
+	for (int i=0; i<adj_list.size(); i++){
+		adj_list_copy.push_back(adj_list[i]);
+	}
+
+	vector<int> stack;
+	vector<int> visited;
+	bool found = false;
+	int current = init_vertex;
+
+	while (!found && !(visited.size() >= nu_vertices)){
+
+		bool already_visited = false;           // Revisar que el nodo actual haya sido visitado
+		for (int i=0; i<visited.size(); i++){
+			if(current == visited[i]) {
+				already_visited = true;
+				break;
+			}
+		}
+
+		// Añadir a los visitados
+		if (!already_visited) visited.push_back(current);
+
+		// En caso de que el nodo actual sea el objetivo
+		if (current == target_vertex){
+			found = true;
+			break;
+		}
+
+		// Borrar los nodos visitados
+		for (int i=0; i<adj_list_copy[current].size(); i++){
+			for (int j=0; j<visited.size(); j++){
+				if(adj_list_copy[current][i] == visited[j])
+					adj_list_copy[current].erase(adj_list_copy[current].begin()+i);
+			}
+		}
+		if (adj_list_copy[current].size() > 0){
+			stack.push_back(current);
+			// Hijo del nodo actual
+			int temp_index = current;
+			current = adj_list_copy[current].back();
+			adj_list_copy[temp_index].pop_back();
+		}
+		else {
+			// En caso de topar con pared
+			current = stack[stack.size()-1];
+			stack.pop_back();
+		}
+	}
+	// Añade el nodo actual para terminar el camino
+	stack.push_back(current);
+
+	// Convertir la lista en string
+	string visited_str = print_vector(visited);
+	string path_str = print_vector(stack);
+	string result = "visited: " + visited_str + "\n" + "path: " + path_str;
+	result = result.substr(0, result.size()-1);
+	return result;
+}
+
+
+string Graph::BFS(int init_vertex, int target_vertex){
+	vector<vector<int>> queue;
+	vector<vector<int>> visited;
+	int current = init_vertex;
+	int parent = -1;
+	bool found = false;
+
+	while (true){
+		// Añadir a visitados
+		visited.push_back(vector<int>({parent, current}));
+
+		// Revisar si es el nodo objetivo
+		if (current == target_vertex){
+			found = true;
+			break;
+		}
+
+		// En caso de que no estén visitados enviarlos a la cola
+		for (int i=0; i<adj_list[current].size(); i++){
+			int val = adj_list[current][i];
+			bool already_visited = false;
+			for (int j=0; j<visited.size(); j++){
+				if (visited[j][1] == val){
+					already_visited = true;
+					break;
+				}
+			}
+			if (!already_visited) queue.push_back(vector<int>({current, val}));
+		}
+		// Tomar nodos de la cola
+		parent = queue[0][0];
+		current = queue[0][1];
+		queue.erase(queue.begin());
+	}
+	// Encontrar el camino
+	vector<int> path;
+	int find = target_vertex;
+	do {
+		for (int i=0; i<visited.size(); i++){
+			if (visited[i][1] == find){
+				path.insert(path.begin(), find);
+				find = visited[i][0];
+			}
+		}
+	} while (find != -1);
+	// Juntar nodos en un mismo arreglo
+	vector<int> converted_visited;
+	for (int i=0; i<visited.size(); i++){
+		converted_visited.push_back(visited[i][1]);
+	}
+
+	// Convertir lista a string
+	string visited_str = print_vector(converted_visited);
+	string path_str = print_vector(path);
+	string result = "visited: " + visited_str + "\n" + "path: " + path_str;
+	result = result.substr(0, result.size()-1);
+	return result;
+}
---
+++ Act 4/main.cpp
@@ -0,0 +1,110 @@
+/*
+*   Rutas de autobus
+*   Autor: Enrique Santos Fraire
+*/
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <cstring>
+#include "graph.h"
+using namespace std;
+
+int main(){
+    // Objetos para clases
+    Graph graph;
+
+    // Declaración de variables
+    int op, partida, llegada, resp;
+    bool salida=false;
+
+    graph.loadGraphList("rutas.txt", 6, 6);
+
+    cout<<"----------Rutas de Autobus----------"<<endl;
+    cout<<endl;
+    cout<<"0 Queretaro\n";
+    cout<<"1 Zacatecas\n";
+    cout<<"2 Aguascalientes\n";
+    cout<<"3 San Luis Potosi\n";
+    cout<<"4 CDMX\n";
+    cout<<"5 Hidalgo\n\n";
+
+    while(salida==false){
+        cout<<"Seleccionar una opcion:\n";
+        cout<<"1) Mostrar rutas\n";
+        cout<<"2) Mostrar camino de un punto a otro\n";
+        cout<<"3) Mostrar camino mas corto de un punto a otro\n";
+        cout<<"4) Salir\n";
+        cin>>op;
+        cout<<endl;
+
+        switch(op){
+        case 1:
+            {
+                // Mostrar las rutas
+                cout<<"----------Ruta de camiones----------"<<endl<<endl;
+                cout<<graph.printAdjList()<<endl<<endl;
+                cout<<"Desea imprimir las rutas?\n";
+                cout<<"1- Si\n2- No\n";
+                cin>>resp;
+                if(resp==1){
+                    ofstream escribe ("Ruta de camiones.txt");
+                    escribe<<graph.printAdjList();
+                    cout<<"Se han guardado las rutas en el archivo Ruta de camiones.txt\n\n";
+                }
+            }
+            break;
+        case 2:
+            {
+                // Camino de un punto a otro
+                cout<<"----------Camino de un punto a otro----------\n\n";
+                cout<<"0 Queretaro\n";
+                cout<<"1 Zacatecas\n";
+                cout<<"2 Aguascalientes\n";
+                cout<<"3 San Luis Potosi\n";
+                cout<<"4 CDMX\n";
+                cout<<"5 Hidalgo\n\n";
+                cout<<"Punto de partida: ";
+                cin>>partida;
+                cout<<"Punto de llegada: ";
+                cin>>llegada;
+                cout<<graph.DFS(partida,llegada);
+                cout<<endl<<endl;
+            }
+            break;
+        case 3:
+            {
+                // Camino de un punto a otro mas rapido
+                cout<<"----------Camino de un punto a otro mas rapido----------\n\n";
+                cout<<"0 Queretaro\n";
+                cout<<"1 Zacatecas\n";
+                cout<<"2 Aguascalientes\n";
+                cout<<"3 San Luis Potosi\n";
+                cout<<"4 CDMX\n";
+                cout<<"5 Hidalgo\n\n";
+                cout<<"Punto de partida: ";
+                cin>>partida;
+                cout<<"Punto de llegada: ";
+                cin>>llegada;
+                cout<<graph.BFS(partida,llegada);
+                cout<<endl<<endl;
+            }
+            break;
+        case 4:
+            {
+                // Salida del programa
+                cout<<"Ha salido del menu"<<endl;
+                salida=true;
+            }
+            break;
+        default:
+            {
+                cout<<"Opcion invalida, intente de nuevo"<<endl;
+            }
+            break;
+        }
+    }
+}
